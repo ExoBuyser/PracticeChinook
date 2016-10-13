9530d2d21d5b6d4c70af340ab39f13bce3da8196
ExoPRESSO_SHOT <ebuyser1@studentmail.nait.ca>
2016-10-13 16:39:01 -0600

    Add additional tracking properties for user

--- ChinookSample/ChinookSystem/Security/ApplicationUser.cs
+++ ChinookSample/ChinookSystem/Security/ApplicationUser.cs
@@ -12,6 +12,7 @@ namespace ChinookSystem.Security
     // You can add User data for the user by adding more properties to your User class, please visit http://go.microsoft.com/fwlink/?LinkID=317594 to learn more.
     public class ApplicationUser : IdentityUser
     {
-        
+        public int? EmployeeID { get; set; }
+        public int? CustomerID { get; set; }
     }
 }
