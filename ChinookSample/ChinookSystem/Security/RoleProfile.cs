dfefdbca1ce3e23e6a52a180371b0486a717b0a8
Exodus Buyser <ebuyser1@studentmail.nait.ca>
2016-10-18 16:08:17 -0600

    setup of profile datasets to be used to contain webpage data

---
+++ ChinookSample/ChinookSystem/Security/RoleProfile.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChinookSystem.Security
+{
+    class RoleProfile
+    {
+        public string RoleID { get; set; }
+        public string RoleName { get; set; }
+        public IEnumerable<string> UserName { get; set; }
+
+    }
+}
---
+++ ChinookSample/ChinookSystem/Security/UserProfile.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChinookSystem.Security
+{
+    public class UserProfile
+    {
+        public string UserID { get; set; } //AspNetUser
+        public string UserName { get; set; } //AspNetUser
+        public int? EmployeeId { get; set; } //AspNetUser
+        public int? CustomerId { get; set; } //AspNetUser
+        public string FirstName { get; set; } //Employee or Customer table
+        public string LastName { get; set; } //Employee or Customer table
+
+        public string Email { get; set; }
+        public string EmailConfirmed { get; set; }
+        public IEnumerable<string> RoleMembership { get; set; }
+    }
+}
